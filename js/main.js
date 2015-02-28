7b8915da7a763cc78b7e942a5924b0cf9f1add79
Joe Crespo <joe@atendesigngroup.com>
2015-02-28 00:07:22 -0800

    sandcamp wip

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 Reveal.addEventListener( 'slidechanged', function( event ) {
     // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-    console.log(event.currentSlide);
+    var $currentSlide = $(event.currentSlide);
+
+    if ($currentSlide.attr('data-html') === 'slides/82-casestudy-1-1.html') {
+    }
 });
